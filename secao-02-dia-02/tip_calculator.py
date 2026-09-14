546d19f128716b807e937c3496a9ed33163a9ce2
Kaan Oliveira <kaanoliveira@users.noreply.github.com>
2026-09-14 19:47:53 -0300

    day 02: remove redundant round from tip calculator

--- secao-02-dia-02/tip_calculator.py
+++ secao-02-dia-02/tip_calculator.py
@@ -10,5 +10,5 @@ people = int(input("How many people to split the bill? "))
 
 total_bill = bill * (1 + tip / 100)
 bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
-print(f"Each person should pay: ${final_amount:.2f}")
+#final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${bill_per_person:.2f}")
